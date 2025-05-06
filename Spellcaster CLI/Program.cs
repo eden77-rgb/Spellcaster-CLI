4899f74cc4bf578b970e29d1ddb7dab08e5bd8ff
Eden Vandewatyne <eden.vandewatyne@estiam.com>
2025-05-06 14:31:33 +0200

    Ajout du fichier .env pour la gestion des clés API

--- Spellcaster CLI/Program.cs
+++ Spellcaster CLI/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using dotenv.net;
+
+class Program 
+{
+    private const string PATH_ENV = ".env";
+    public static void Main(string[] args)
+    {
+        DotEnv.Load(options: new DotEnvOptions(envFilePaths: new[] { PATH_ENV }));
+
+        var apiKey = Environment.GetEnvironmentVariable("API");
+
+        Console.WriteLine(apiKey);
+    }
+} 
